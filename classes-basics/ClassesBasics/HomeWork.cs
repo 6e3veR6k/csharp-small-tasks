6704e13beb21498f3c593b34d97b6f05c966699f
6e3veR6uK <bezyn4uk@gmail.com>
2023-05-05 23:29:25 +0300

    Finished H1T2

--- classes-basics/ClassesBasics/HomeWork.cs
+++ classes-basics/ClassesBasics/HomeWork.cs
@@ -33,5 +33,32 @@ namespace ClassesBasics
             Console.WriteLine($"I live at the address of\n\tAp. {homeAddress.Apartment}, {homeAddress.House} {homeAddress.Street} St.\n\t{homeAddress.City}, {homeAddress.Country} {homeAddress.Index}");
             Console.WriteLine(new string('-', 40));
         }
+
+
+        public static void TaskBravoRun()
+        {
+            //Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит  на экран периметр и площадь.
+            double firstRectangleSide = GetValidInputValue("Enter first side of rectangle: ");
+            double secondRectangleSide = GetValidInputValue("Enter second side of rectangle: ");
+
+            Rectangle userRectangle = new Rectangle(firstRectangleSide, secondRectangleSide);
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Your rectangle's area is equal to {userRectangle.Area:F3} and its perimetr is equal {userRectangle.Perimetr:F3}");
+
+        }
+
+        private static double GetValidInputValue(string messageForInput)
+        {
+            double userInputValue;
+            Console.Write(messageForInput);
+
+            while (!double.TryParse(Console.ReadLine(), out userInputValue) || userInputValue <= 0)
+            {
+                Console.WriteLine("You entered wrong data...");
+                Console.Write(messageForInput);
+            }
+
+            return userInputValue;
+        }
     }
 }
