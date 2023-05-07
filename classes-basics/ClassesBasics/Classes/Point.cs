19bc91992569df642d13d5e526accda2e6993f29
6e3veR6uK <bezyn4uk@gmail.com>
2023-05-07 15:44:05 +0300

    Finished H1T4

---
+++ classes-basics/ClassesBasics/Classes/Point.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassesBasics.Classes
+{
+    internal class Point
+    {
+        private int xCoordinate;
+        private int yCoordinate;
+        private string name;
+
+        public int XCoordinate
+        {
+            get { return xCoordinate; }
+        }
+
+        public int YCoordinate
+        {
+            get { return yCoordinate; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+
+        public Point(string name, int xCoordinate, int yCoordinate)
+        {
+            this.name = name;
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+        }
+    }
+}
